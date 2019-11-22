24d216fa09b925f368b5c1d54c72392e38b7d8ca
Kristian Garza <kj.garza@gmail.com>
2019-11-22 07:12:54 +0100

    file cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LinksTabs doi="10.5438/0010"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LinksTabs from './components/LinksTabs.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    LinksTabs
   }
 }
 </script>
